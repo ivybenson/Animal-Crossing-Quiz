91ccd6098950e79076ede78ac20056d9c924ae7c
ivybenson <68450827+ivybenson@users.noreply.github.com>
2020-08-02 15:44:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const STORE = [
+    {
+    question:'What was gaming system was Animal Crossing originally made for?', 
+    answers:['Game Cube', 'Nintendo Switch', 'N64', 'Nintendo Wii'], 
+    correctAnswer:'N64'
+    },
+    {question:'Villagers can get infested with:' , 
+    answers:['Ants','Fleas','Flies','Beetles'], 
+    correctAnswer:'Fleas'
+    },
+    {question:'In Animal Crossing: New Horizons Tom Nook is:' , 
+    answers:['President','Mayor','Land Developer and Owner of Nook Inc', 'Philanthropist'], 
+    correctAnswer:'Land Developer and Owner of Nook Inc'
+    },
+    {question:'Daisie Mae runs:', 
+    answers:['Sow Joans Stalk Market', 'Dow Jones Stock Market','Vegetable Shop','Sow Joans Turnip Market'], 
+    correctAnswer:'Sow Joans Stalk Market'
+    },
+    {question:'Blathers, the owl who runs the museum hates to collect:' , 
+    answers:['Fossils','Fish','Bugs','Paintings'], 
+    correctAnswer:'Bugs'
+    },
+    {question:'Which Animal Crossing non-player character is a playable character in Super Smash Bros Ultimate(2018)', 
+    answers:['Mable','Blathers','Tom Nook','Isabelle'], 
+    correctAnswer:'Isabelle'
+    }
+  ];
